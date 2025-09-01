d896f719afb2a1f2b403568e73ab2db1955f5a49
Ankit Mishra <ankitmishra221208@gmail.com>
2025-09-01 21:37:21 +0530

    Initial commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "2787df62a8535104d6c0ffe4ebb15b9c",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
